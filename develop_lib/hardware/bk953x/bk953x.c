7a0ce2ec419af92a5fb0945ecd7978bb5173313f
chenxiaonian <1012353276@qq.com>
2021-10-26 00:30:41 +0800

    -- 完善bk953x的驱动接口

--- develop_lib/hardware/bk953x/bk953x.c
+++ develop_lib/hardware/bk953x/bk953x.c
@@ -2,8 +2,32 @@
 
 #include "user_board.h"
 #include "middle_api.h"
-
+#include "util.h"
 #include "bk953x.h"
 
 
+int bk953x_sample_rate_set(bk953x_object_t *p_bk953x_object,bk953x_sample_rate_e rate)
+{
+
+}
+
+int bk953x_band_type_set(bk953x_object_t *p_bk953x_object,bk953x_band_type_e band_type)
+{
+
+}
+
+int bk953x_power_set(bk953x_object_t *p_bk953x_object, uint8_t power)
+{
+
+}
+
+int bk953x_freq_set(bk953x_object_t *p_bk953x_object, uint32_t freq)
+{
+
+}
+
+int bk953x_chan_set(bk953x_object_t *p_bk953x_object,uint16_t chan)
+{
+
+}
 
--- develop_lib/hardware/bk953x/bk953x.h
+++ develop_lib/hardware/bk953x/bk953x.h
@@ -1,13 +1,29 @@
 #ifndef __BK953X_H
 #define __BK953X_H
 
+
+typedef enum
+{
+    BK953X_SAMPLE_RATE_48K,
+    BK953X_SAMPLE_RATE_32K,
+    BK953X_SAMPLE_RATE_24K,
+    BK953X_SAMPLE_RATE_12K,
+} bk953x_sample_rate_e;
+
+typedef enum
+{
+    BK953X_BAND_V,  //V段
+    BK953X_BAND_U   //U段
+} bk953x_band_type_e;
+
+
 typedef struct 
 {
     i2c_type_e i2c_type;
     uint8_t    device_id;
     uint16_t   chip_id;
-} bk953x_t;
-
+    bk953x_band_type_e band_type;
+} bk953x_object_t;
 
 
 #endif
